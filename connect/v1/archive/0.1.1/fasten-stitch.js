9fcf9fd1e470123a58ca6d95142f1d8d7957ad3c
fastenhealth <jason@thesparktree.com>
2024-05-17 18:35:08 +0000

    Update from https://github.com/fastenhealth/fasten-connect-stitch/commit/affac17c349e9a03d33fe89a84b3080786227233

--- connect/v1/archive/0.1.1/fasten-stitch.js
+++
@@ -1,3 +0,0 @@
-import "./main.69b381c4c5812c8a.js";
-import "./polyfills.92abcf9c56b01a74.js";
-import "./runtime.4162e9c8c3dc90f2.js";
--- connect/v1/fasten-stitch.js
+++ connect/v1/fasten-stitch.js
@@ -1,3 +1,3 @@
-import "./main.69b381c4c5812c8a.js";
+import "./main.4907acff7c3c54c3.js";
 import "./polyfills.92abcf9c56b01a74.js";
 import "./runtime.4162e9c8c3dc90f2.js";
