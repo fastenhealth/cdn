a2ff77a7db8a515ff89c654a36404531da58d7ff
fastenhealth <jason@thesparktree.com>
2025-03-09 01:32:39 +0000

    Update from https://github.com/fastenhealth/fasten-connect-frontend-ngx/commit/569a9e3234ae28e61c8dae76f80076d1777ea91b

--- connect/dev-element-dev/main.js
+++ connect/dev-element-dev/main.js
@@ -208,21 +208,10 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "environment": () => (/* binding */ environment)
 /* harmony export */ });
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 const environment = {
-    name: "local",
-    embed_endpoint_base: 'https://localhost:4201',
+    name: "development",
+    embed_endpoint_base: 'https://embed.connect-dev.fastenhealth.com',
 };
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
 
 
 /***/ }),
