930ade66dee6c9b3469c7c91bf47831af1de8681
fastenhealth <jason@thesparktree.com>
2025-04-30 02:32:50 +0000

    Update from https://github.com/fastenhealth/fasten-connect-stitch/commit/fcb479114099c4342f95a3d08058b131b4ff7a26

--- connect/dev/main.js
+++ connect/dev/main.js
@@ -1032,9 +1032,6 @@ class FastenStitchComponent {
       this.errorMessage = 'Could not register Fasten Connect installation using id. Please contact the developer of this app.';
       console.log("Invalid Fasten Connect registration", err);
     });
-    this.orgConnectionCallback.emit({
-      error: "initializing-fasten-stitch"
-    });
     this.messageBus.orgConnectionCallbackSubject.subscribe(orgConnectionCallbackData => {
       if (!orgConnectionCallbackData) {
         console.warn("filter empty orgConnectionCallbackData");
